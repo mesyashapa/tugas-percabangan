d6b84a349a7fba30d18c4da0e83416891fb3c989
mesyashapa <mesyashapaa@gmail.com>
2024-10-09 16:19:04 +0700

    Update TugasPercabangan_Mesya Shapa Ulhaq_607012400132.java

--- TugasPercabangan.java
+++ TugasPercabangan.java
@@ -1,19 +1,71 @@
 import java.util.Scanner;
 
-public class TugasPercabangan{
-    public static void main(String[] args){
-        int lamaKerja, jumlahJamKerja;
-        double totalGaji;
+public class TugasPercabangan {
+    public static void main(String[] args) {
         
+        int GAJI_POKOK = 5000000;
+        int UANG_LEMBUR_PER_JAM = 50000;
+        int masaKerja, jamKerja, jamLembur;
+        double tambahan, totalGaji;
 
+        
         Scanner scanner = new Scanner(System.in);
-        lamaKerja       = scanner.nextInt();
-        jumlahJamKerja  = scanner.nextInt();
 
-        /* 
-            Tuliskan proses perhitungan gaji di sini
-        */
         
-        System.out.println(totalGaji);
+        System.out.print("Masukkan masa kerja (dalam tahun): ");
+        masaKerja = scanner.nextInt();
+        System.out.print("Masukkan jumlah jam kerja dalam 1 bulan: ");
+        jamKerja = scanner.nextInt();
+
+        
+        if (jamKerja > 40) { // 240 jam = 8 jam/hari * 30 hari
+            jamLembur = jamKerja - 40;
+        } else {
+            jamLembur = 0;
+        }
+
+        
+        if (masaKerja < 5) {
+            tambahan = 0;
+        } else if (masaKerja >= 5 && masaKerja <= 10) {
+            tambahan = GAJI_POKOK * 0.05; // Tambahan 5%
+        } else {
+            tambahan = GAJI_POKOK * 0.10; // Tambahan 10%
+        }
+
+        
+        totalGaji = GAJI_POKOK + tambahan + (jamLembur * UANG_LEMBUR_PER_JAM);
+
+        
+        System.out.println("Total gaji yang diterima adalah: Rp " + totalGaji);
+
+        
+        scanner.close();
     }
 }
+
+
+
+// Deklarasi
+// Var gajiPokok = 5000000;
+//     uangLembur = 50000;
+//     masaKerja;
+//     jamKerja;
+//     jumlahJamKerja;
+//     jumlahJamLembur;
+//     : integer;
+//     totalGaji;
+//     bonus;
+//     : double;
+
+// Algoritma
+//          input(masaKerja);
+//          input(jamKerja);
+// if (masa kerja <5 tahun) then
+//  output (bonus = 0);
+// else if (masa kerja>= 5 && masaKerja <= 10 ) then 
+//  output (bonus = 0,05 *uangLembur);
+//  else(masa kerja > 10 tahun) then
+//  output (bonus = 0,01)
+
+// End
